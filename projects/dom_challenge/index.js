df2ece596eb97ff167c233cc8489e07d8e723fe1
Aryaman Singla <aryamansingla199@gmail.com>
2022-05-27 16:03:50 +0530

    all files deleted

--- projects/dom_challenge/index.js
+++
@@ -1,5 +0,0 @@
-document.querySelectorAll('.list')[2].innerHTML += " changed by JS";
-
-document.querySelector('button').addEventListener('click', function () {
-  document.querySelector('h1').classList.toggle('huge');
-})
--- projects/intro_to_node/index.js
+++
@@ -1,8 +0,0 @@
-var superheroes = require("superheroes");
-var villain = require("supervillains");
-
-var mySuperHeroName = superheroes.random();
-var myVillainName = villain.random();
-
-console.log("Hero: " + mySuperHeroName);
-console.log("Villain: " + myVillainName);
